ee316e47eade29a89d2a21da843972eb0a01aebe
Gabriel Jonay Vera Estévez <alu0101398198@ull.edu.es>
2023-03-01 09:28:35 +0100

    Test added

--- src/logging-espree.js
+++ src/logging-espree.js
@@ -1,16 +1,46 @@
-import * as escodegen from "escodegen";
-import * as espree from "espree";
-import * as estraverse from "estraverse";
-import * as fs from "fs/promises";
+import * as escodegen from 'escodegen';
+import * as espree from 'espree';
+import * as estraverse from 'estraverse';
+import * as fs from 'fs/promises';
 
 export async function transpile(inputFile, outputFile) {
-  // Fill in the code here
+  let input = await fs.readFile(inputFile, 'utf-8')
+  let output = addLogging(input);
+  if (outputFile === undefined) {
+      console.log(output);
+      return;
+  }
+  await fs.writeFile(outputFile, output)
 }
 
 export function addLogging(code) {
-  // Fill in the code here
+  const ast = espree.parse(code, { ecmaVersion: 12, loc: true });
+  estraverse.traverse(ast, {
+    enter: function(node, parent) {
+      if (node.type === 'FunctionDeclaration' ||
+          node.type === 'FunctionExpression' || 
+          node.type === 'ArrowFunctionExpression') {
+        addBeforeCode(node);
+      }
+    }
+  });
+  return escodegen.generate(ast);
 }
 
 function addBeforeCode(node) {
- // Fill in the code here
+  const name = node.id ? node.id.name : '<anonymous function>';
+  const params = node.params.map(p => '${ ' +  p.name + ' }').join(', ');
+  const beforeCode = 'console.log(\`Entering ' + name + '(' + params + ') at line ' + node.loc.start.line + '\`);';
+  const beforeNodes = espree.parse(beforeCode, { ecmaVersion: 12 }).body;
+  node.body.body = beforeNodes.concat(node.body.body);
 }
+
+//console.log(addLogging(`
+//function foo(a, b) {   
+//  var x = 'blah';   
+//  var y = (function () {
+//    return 3;
+//  })();
+//}
+//foo(1, 'wut', 3);
+//`));
--- test/test.mjs
+++ test/test.mjs
@@ -1,26 +1,44 @@
-import { transpile } from "../src/logging-espree.js";
 import assert from 'assert';
-import * as fs from "fs/promises";
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import * as fs from 'fs/promises';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+
+import {transpile} from '../src/logging-espree.js';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 import Tst from './test-description.mjs';
 
 const Test = Tst.map(t => ({
-  input: __dirname + '/data/' + t.input,
-  output: __dirname + '/data/' + t.output,
-  correctLogged: __dirname + '/data/' + t.correctLogged,
-  correctOut: __dirname + '/data/' + t.correctOut,
-})
-)
+                       input: __dirname + '/data/' + t.input,
+                       output: __dirname + '/data/' + t.output,
+                       correctLogged: __dirname + '/data/' + t.correctLogged,
+                       correctOut: __dirname + '/data/' + t.correctOut,
+                     }))
 
 function removeSpaces(s) {
   return s.replace(/\s/g, '');
 }
 
 for (let i = 0; i < Test.length; i++) {
-  // Write your tests here   
+  it ('Test ' + i, async function () {
+    const t = Test[i];
+    await transpile(t.input, t.output);
+    const correctLogged = await fs.readFile(t.correctLogged, 'utf-8');
+    assert.equal(
+        removeSpaces(correctLogged),
+        removeSpaces(await fs.readFile(t.output, 'utf-8')));
+
+    let outputs = [];
+    let oldLog = console.log;
+    console.log =
+        function(...args) {
+      outputs.push(args.join(' '));
+    }
+
+    await import(t.output);
+    outputs.push('');  // Add a newline at the end.
+    console.log = oldLog;
+    const correctOut = await fs.readFile(t.correctOut, 'utf-8');
+    assert.equal(outputs.join('\n'), correctOut);
+  });
 }
-
-
